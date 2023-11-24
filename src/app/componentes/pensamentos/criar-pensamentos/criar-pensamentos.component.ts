acc9a0ca2b8e94e63cf1ddde830aa30f0fd1d3a8
Elaine <e.cristina00@gmail.com>
2023-11-23 23:10:59 -0300

    Property Binding

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamentos',
   templateUrl: './criar-pensamentos.component.html',
   styleUrls: ['./criar-pensamentos.component.scss']
 })
-export class CriarPensamentosComponent {
+export class CriarPensamentosComponent implements OnInit{
+
+  pensamento = {
+    id: '1',
+    conteudo: ' Aqui vai seu conteúdo',
+    autoria: 'Aqui vai a autoria',
+    modelo: ''
+
+  }
+
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
