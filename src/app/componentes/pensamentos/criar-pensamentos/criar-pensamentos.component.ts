49a7c1abe26099d515279cce80953b0da2407e67
Elaine <e.cristina00@gmail.com>
2023-12-19 22:06:22 -0300

    Validators required

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Route, Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamentos',
@@ -29,16 +29,20 @@ export class CriarPensamentosComponent implements OnInit{
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulario reativo'],
-      autoria: ['Agular'],
+      conteudo: ['', [Validators.required]],
+      autoria: ['', [Validators.required]],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento(){
-    this.service.criar(this.formulario.value).subscribe(() => {
-    this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario)
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+        })
+    }
+
   }
 
   cancelar(){
