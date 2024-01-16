1b8e349e01cef76a33300a3aafcdab6341137df9
Elaine <e.cristina00@gmail.com>
2024-01-16 01:03:49 -0300

    Recarregando um componente.

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,6 +1,7 @@
 import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -16,7 +17,11 @@ export class ListarPensamentosComponent implements OnInit {
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = []
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+
+    ) { }
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
@@ -44,6 +49,17 @@ export class ListarPensamentosComponent implements OnInit {
       .subscribe(listaPensamentos => {
         this.listaPensamentos = listaPensamentos
       })
+  }
+
+  recarregarComponente(){
+
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    // location.reload();
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
+
   }
 
   listarFavoritos(){
